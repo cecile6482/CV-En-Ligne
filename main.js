7e9025cfcb48460e4fdf8bf5494d12685d547712
cecile6482 <cecile.elshtby@gmail.com>
2022-04-12 12:17:46 +0200

    init

---
+++ main.js
@@ -0,0 +1,22 @@
+const options = {
+    bottom: '90%', // default: '32px'
+    right: '20px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#ccffcc', // default: '#fff'
+    backgroundColor: 'none',  // default: '#fff'
+    buttonColorDark: '#2F4F4F',  // default: '#100f2c'
+    buttonColorLight: '#FFFAF0', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
+  
+  const darkmode = new Darkmode(options);
+  darkmode.showWidget();
+
+
+
+  function phoneNumber() {
+    alert("Contactez-moi au 07 53 23 35 22")
+}
