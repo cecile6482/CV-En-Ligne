09a14e1fc33fa2f04492dae1fd4e7ac3d62ec494
cecile6482 <cecile.elshtby@gmail.com>
2022-04-17 13:51:07 +0200

    màj optimization 3.8

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const options = {
     right: '20px', // default: '32px'
     left: 'unset', // default: 'unset'
     time: '0.5s', // default: '0.3s'
-    mixColor: '#97b1d4', // default: '#fff'
+    mixColor: '#d0aea5', // default: '#fff'
     backgroundColor: 'none',  // default: '#fff'
     buttonColorDark: '#9a8385',  // default: '#100f2c'
     buttonColorLight: '#FFFAF0', // default: '#fff'
